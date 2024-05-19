8f8c03f084c8872edd38938da5dd7b42b6ffa524
pctronique <pctronique@free.fr>
2024-05-19 10:30:24 +0200

    del folder 001

--- 001/require.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p projecttmp/require
-cd projecttmp/require
-curl -sL https://github.com/pctronique/pctrouting/archive/refs/tags/1.0.0.tar.gz | tar xz
-cp -r pctrouting-1.0.0/project/www/src/class/pctrouting ../../project/www/src/class/
-cd ../..
-rm -rf projecttmp/require
